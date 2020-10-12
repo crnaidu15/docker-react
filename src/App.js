81fb660e81493e84ef0dfbeb95303e5f8dd26658
Reddaiah Challa <reddaiah.challa@hexagonsi.com>
2020-10-12 16:07:26 +0530

    Iinitial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. It is easy.
         </a>
       </header>
     </div>
